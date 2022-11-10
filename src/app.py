6004d176fe590f3769d43f06167303b740fee041
SiyaKamboj <siyakamboj20@gmail.com>
2022-11-10 08:24:54 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 #This is the python file for the streamlit app
 import streamlit as st
+import pandas as pd
 
 header = st.container()
 dataset = st.container()
@@ -14,8 +15,17 @@ with dataset:
     st.header('Credit Card Fraud Dataset')
     st.text('This dataset is from Kaggle and PCA. Here is the EDA report...')
 
+    credit_card_data = pd.read_csv("creditcard.csv")
+    st.write(credit_card_data.head())
+
+    st.subheader('Fraud (1) or Not Fraud (0) Distribution in Dataset')
+    fraudOrNot= pd.DataFrame(credit_card_data['Class'].value_counts())
+    st.bar_chart(fraudOrNot)
+
 with timeline:
     st.header('Timeline of project')
+
+    st.markdown('* **first subproject!** This subproject is ....')
 
 with modelTraining:
     st.header('Time to train model')
