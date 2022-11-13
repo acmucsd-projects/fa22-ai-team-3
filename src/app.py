3c42019937bb103461ea5a454d4601b9b7642f4e
SiyaKamboj <siyakamboj20@gmail.com>
2022-11-13 10:32:31 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -29,3 +29,8 @@ with timeline:
 
 with modelTraining:
     st.header('Time to train model')
+    st.text('Choose which hyperparamaters you want to use')
+    
+    time_col, V1 = st.columns(2)
+    time_elapsed= time_col.slider('How much time has elapsed since purchase 1?', min_value=0, max_value=20000, value=20, step=2)
+    
