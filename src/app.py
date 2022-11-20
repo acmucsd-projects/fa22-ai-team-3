2d72e6d7f7c4d637d4310162ed514de1dfaafc96
Rohan Nambimadom <rohan.nambimadom@gmail.com>
2022-11-20 14:34:37 -0800

    take in xgboost model and predictions on notebook

--- src/app.py
+++ src/app.py
@@ -11,7 +11,16 @@ from sklearn.pipeline import Pipeline
 from pprint import pprint
 from sklearn.model_selection import train_test_split
 import random
+import pickle
+import joblib
 
+#model = joblib.load(r"C:\\Users\\rohan\\fa22-ai-team-3\\models\\XGBoost.pkl")
+def load_model():
+    with open('../models/XGBoost.pkl', 'rb') as file:
+        data = joblib.load(file)
+    return data
+
+model = load_model()
 
 # header = st.container()
 # dataset = st.container()
@@ -20,28 +29,25 @@ import random
 
 
 
-pipe = Pipeline([('standardScaler', StandardScaler()), 
-                 ('quantiletransformer', QuantileTransformer()), 
-                 ('logisticRegression', LogisticRegression())])
-df = pd.read_csv(r"C:\\Users\\rohan\\OneDrive\\Desktop\\fa22-ai-team-3\\input\\creditcard.csv")
+df = pd.read_csv(r"C:\Users\rohan\fa22-ai-team-3\input\creditcard.csv")
 df = df.rename(columns={'Class': 'Fraud'})
 df['Fraud'] = df['Fraud'].astype(int)
 
 X = df.drop(['Fraud'], axis = 1)
 Y = df["Fraud"]
 
-xData = X.values
-yData = Y.values
-xTrain, xTest, yTrain, yTest = train_test_split(
-        xData, yData, test_size = 0.2, random_state = 42)
+# xData = X.values
+# yData = Y.values
+# xTrain, xTest, yTrain, yTest = train_test_split(
+#         xData, yData, test_size = 0.2, random_state = 42)
 
-pipe = Pipeline([
-    ('standardScaler', StandardScaler()), 
-    ('quantiletransformer', QuantileTransformer()), 
-    ('logisticRegression', LogisticRegression())
-])
+# pipe = Pipeline([
+#     ('standardScaler', StandardScaler()), 
+#     ('quantiletransformer', QuantileTransformer()), 
+#     ('logisticRegression', LogisticRegression())
+# ])
 
-pipe.fit(xTrain, yTrain)
+# pipe.fit(xTrain, yTrain)
 
 st.title("Title")
 st.text("String of SIya's work gone: here is its remains")
@@ -50,15 +56,13 @@ age = st.slider("age", 0, 100)
 
 st.text(f"your age is: {age}")
 #trying to add in random predictions of random row to app
-# row = df.iloc[random.randint(0, df.shape[0])]
-# row = row.drop('Class')
-# # print(row)
+row = df.iloc[random.randint(0, df.shape[0])]
+row = row.drop('Fraud')
+print(row)
 
-# row = np.expand_dims(row, axis = 1)
+row = np.expand_dims(row, axis = 0)
 
-# prediction = pipe.predict(row)
-# st.text(f"prediction: {prediction}")
+prediction = model.predict(row)
+st.text(f"prediction: {prediction}")
 
-# print("IT WORKS!")
-
-
+print("IT WORKS!")
